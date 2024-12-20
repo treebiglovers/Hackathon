a7cc90c42269170114360ace1c7b29819472b3b7
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-12-21 07:23:31 +0800

    Removed duplicate enum

--- common/src/dtos/members/listings/MemberListingWithOwningMemberDTO.ts
+++ common/src/dtos/members/listings/MemberListingWithOwningMemberDTO.ts
@@ -1,15 +1,6 @@
 import { z as zod } from "zod";
-import { OptionalEntityBaseSchema } from "../../EntityBaseSchema";
-import { LimitConstants } from "../../../constants/LimitConstants";
 import { MemberListingDTOSchema } from "./MemberListingDTO";
 import { MemberDTOSchema } from "../MemberDTO";
-
-export enum MemberListingState
-{
-    AVAILABLE = 0,
-    RESERVED = 1,
-    CLOSED = 2,
-}
 
 export const MemberListingWithOwningMemberDTOSchema = MemberListingDTOSchema.extend(
 {     
