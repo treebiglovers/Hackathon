d8187b456c9fa9c59451849d119c7e5145076422
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-12-22 09:26:47 +0800

    ZZZ

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Box, Text } from "@mantine/core";
 import { useMemberData } from "@app/services/MemberService.ts";
+import { Listings } from "@app/components/listings/Listings.tsx";
 
 export const Route = createFileRoute('/')({
   component: () =>
   {
-      const memberData = useMemberData(
-          state => state.memberData,
-      );
+      // const memberData = useMemberData(
+      //     state => state.memberData,
+      // );
+      //
+      // return (
+      //     <Box>
+      //         <Text>Listing</Text>
+      //         {memberData !== null ? <Text>Logged in</Text> : <Text>Not logged in</Text>}
+      //     </Box>
+      // );
       
-      return (
-          <Box>
-              <Text>Listing</Text>
-              {memberData !== null ? <Text>Logged in</Text> : <Text>Not logged in</Text>}
-          </Box>
-      );
+      return <Listings />;
   }
 });
